f1e1cf2a2d09e609a73111024327d28d2dd82cc9
Gabriele Armento <gabriele.armento@gmail.com>
2023-02-05 00:00:14 +0100

    Add shadow to trout

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,14 @@ function App() {
     setCount(count + 1);
     // Animate the trout by moving it up and down once.
     const trout = document.querySelector("img");
+    const shadow = document.querySelector("#shadow");
     trout.style.transform = "translateY(-10px)";
+    shadow.style.transform = "translateY(10px)";
+    shadow.style.transform = "scale(1.2)";
     setTimeout(() => {
       trout.style.transform = "translateY(0px)";
+      shadow.style.transform = "translateY(0px)";
+      shadow.style.transform = "scale(1)";
     }, 100);
   }
   return (
@@ -18,8 +23,17 @@ function App() {
       <h1 className="text-2xl  mb-4">
         {count === 0 ? "Tap the trout!" : `You tapped ${count} times!`}
       </h1>
-      <button onClick={handleClick}>
-        <img src="/trout.png" className="w-64 h-auto transform" />
+      <button onMouseDown={handleClick}>
+        <img
+          src="/trout.png"
+          className="w-64 h-auto transform z-50 select-none"
+        />
+        <div className="flex flex-col z-0 items-center justify-center skew-y-[60deg] -rotate-[65deg] -translate-y-1/3">
+          <div
+            id="shadow"
+            class="w-24 h-24 rounded-full bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-stone-900 via-white opacity-10"
+          ></div>
+        </div>
       </button>
     </div>
   );
